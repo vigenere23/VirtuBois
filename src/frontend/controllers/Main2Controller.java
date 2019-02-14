773779d3af80f0c0ea5627687ccbc8ad7f521668
jordan.mayhue.1 <jordan.mayhue.1@gmail.com>
2019-02-13 21:35:39 -0500

    Added Main2 view and controller for testing purposes

---
+++ src/frontend/controllers/Main2Controller.java
@@ -0,0 +1,27 @@
+package frontend.controllers;
+
+import helpers.JavafxHelper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main2Controller implements IController {
+
+    private Stage stage;
+
+    @FXML AnchorPane root;
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void handleMenuFileExit(ActionEvent actionEvent) {
+        // TODO ask for saving
+        JavafxHelper.quitApplication();
+    }
+
+    public void handleMenuHelpAbout(ActionEvent actionEvent) {
+        JavafxHelper.addView("About", "About", false);
+    }
+}

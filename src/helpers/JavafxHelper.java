a1211d6542e72551d6f0221e4d24e0a25359878b
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-02-15 13:22:42 -0500

    small fix + added out/ to .gitignore (but not .jar)

--- src/helpers/JavafxHelper.java
+++ src/helpers/JavafxHelper.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import presentation.controllers.IController;
 
 public class JavafxHelper {
   public static void loadView(Stage stage, String viewName, String title, boolean maximised) {

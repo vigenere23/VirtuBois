f9e5bd70635f9e31c8c5e047453450a42b761b52
jordan.mayhue.1 <jordan.mayhue.1@gmail.com>
2019-02-14 21:59:40 -0500

    Added Main2 view and controller for testing purposes

--- src/frontend/controllers/Main2Controller.java
+++ src/frontend/controllers/Main2Controller.java
@@ -1,17 +1,10 @@
 package frontend.controllers;
 
 import helpers.JavafxHelper;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.scene.control.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Observable;
 
 
 public class Main2Controller implements IController {

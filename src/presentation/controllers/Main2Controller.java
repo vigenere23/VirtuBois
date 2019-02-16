19ec681ed3d73ab35ba0f59870bb165297d92584
jordan.mayhue.1 <jordan.mayhue.1@gmail.com>
2019-02-15 23:05:35 -0500

    Added TextFlow protoype for pack properties disp.

--- src/presentation/controllers/Main2Controller.java
+++ src/presentation/controllers/Main2Controller.java
@@ -4,6 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class Main2Controller extends BaseController implements IController {
@@ -12,6 +16,11 @@ public class Main2Controller extends BaseController implements IController {
 
     @FXML
     public ListView listView;
+    @FXML
+    public TextFlow packProp;
+    @FXML
+    public TextFlow packPropValues;
+
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -22,5 +31,13 @@ public class Main2Controller extends BaseController implements IController {
     {
         ObservableList<String> listItems = FXCollections.observableArrayList("Allo","Bonjour");
         listView.setItems(listItems);
+
+        Text packInfo = new Text("Code barre : \nLongeur : \nLargeur : \nHauteur : \nDate de production : \nHeure de production : \nType : ");
+        packInfo.setFont(new Font(15));
+        packInfo.setFill(Color.WHITESMOKE);
+        packProp.setLineSpacing(40.0);
+        packProp.getChildren().add(packInfo);
+
+
     }
 }

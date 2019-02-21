b166e23bbc82cf9b2124ef1497cb7b8f8ba0d10c
GASTP33 <gabriel.st-pierre.6@ulaval.ca>
2019-02-21 18:25:04 -0500

    removed unused view

--- src/presentation/javafxModels/YardModel.java
+++ src/presentation/javafxModels/YardModel.java
@@ -1,13 +1,16 @@
 package presentation.javafxModels;
 
+import javafx.scene.layout.Pane;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class YardModel implements IModel {
+public class YardModel extends Pane implements IModel {
     private List<PackModel> packModels;
     private ChargerModel chargerModel;
 
     public YardModel() {
+        super();
         packModels = new ArrayList<>();
     }
 

c5ff4cad1061b6f6852761ecf2a076734dc894aa
MartDesc <martinedeschenes@hotmail.ca>
2019-03-29 14:16:28 -0400

    elevation view 2.0 functionnal (cest vraiment fucking magnifique)

--- src/presentation/presenters/ElevationViewPresenter.java
+++ src/presentation/presenters/ElevationViewPresenter.java
@@ -46,22 +46,24 @@ public class ElevationViewPresenter extends Pane implements IPresenter {
     private void drawBundles() {
         double height = getHeight();
         double width = getWidth();
-        double minLength = 0;
-        double maxLength = 0;
-        double maxHeight = 0;
+        double minLength = allBundles.get(0).getX() - (allBundles.get(0).width/2);
+        double maxLength = allBundles.get(0).getX() + (allBundles.get(0).width/2);
+        double maxHeight = allBundles.get(0).z + allBundles.get(0).height;
         for (BundleDto bundle : allBundles) {
-            if ((bundle.position.getX() - (bundle.length / 2)) < minLength) {
-                minLength = bundle.position.getX() - (bundle.length / 2);
+            if ((bundle.position.getX()-(bundle.width/2)) < minLength){
+                minLength = bundle.position.getX() - (bundle.width/2);
             }
-            if ((bundle.position.getX() + (bundle.length / 2) < maxLength)) {
-                maxLength = bundle.position.getX() + (bundle.length / 2);
+            if((bundle.position.getX() + (bundle.width/2) > maxLength)){
+                maxLength = bundle.position.getX() + (bundle.width/2);
             }
             if ((bundle.height + bundle.z > maxHeight)){
                 maxHeight = bundle.height + bundle.z;
             }
         }
-        double scaleZ = height / maxHeight;
-        double scaleX = width / maxLength;
+
+
+        double scaleZ = height/maxHeight;
+        double scaleX = width/(maxLength - minLength);
 
         for (BundleDto presenter : allBundles) {
             double xPos = 0;
@@ -79,8 +81,8 @@ public class ElevationViewPresenter extends Pane implements IPresenter {
             Rectangle rectangle = bundlePresenter.getRectangle();
             rectangle.setWidth(presenter.width*scaleX);
             rectangle.setHeight(presenter.height*scaleZ);
-            rectangle.setX(xPos - rectangle.getWidth() / 2);
-            rectangle.setY(zPos - rectangle.getHeight() / 2);
+            rectangle.setX(xPos - rectangle.getWidth()/2);
+            rectangle.setY(zPos - rectangle.getHeight());
             getChildren().add(rectangle);
         }
     }

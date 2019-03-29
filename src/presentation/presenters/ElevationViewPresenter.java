18ce8368e52791f4c3dc9e0d4e291c289135f683
MartDesc <martinedeschenes@hotmail.ca>
2019-03-29 14:40:13 -0400

    elevation view working on Y axis

--- src/presentation/presenters/ElevationViewPresenter.java
+++ src/presentation/presenters/ElevationViewPresenter.java
@@ -43,36 +43,35 @@ public class ElevationViewPresenter extends Pane implements IPresenter {
         draw();
     }
 
-    private void drawBundles() {
+    private void drawBundlesAxisX() {
         double height = getHeight();
         double width = getWidth();
-        double minLength = allBundles.get(0).getX() - (allBundles.get(0).width/2);
-        double maxLength = allBundles.get(0).getX() + (allBundles.get(0).width/2);
+        double minWidth = allBundles.get(0).getX() - (allBundles.get(0).width/2);
+        double maxWidth = allBundles.get(0).getX() + (allBundles.get(0).width/2);
         double maxHeight = allBundles.get(0).z + allBundles.get(0).height;
         for (BundleDto bundle : allBundles) {
-            if ((bundle.position.getX()-(bundle.width/2)) < minLength){
-                minLength = bundle.position.getX() - (bundle.width/2);
+            if ((bundle.position.getX()-(bundle.width/2)) < minWidth){
+                minWidth = bundle.position.getX() - (bundle.width/2);
             }
-            if((bundle.position.getX() + (bundle.width/2) > maxLength)){
-                maxLength = bundle.position.getX() + (bundle.width/2);
+            if((bundle.position.getX() + (bundle.width/2) > maxWidth)){
+                maxWidth = bundle.position.getX() + (bundle.width/2);
             }
             if ((bundle.height + bundle.z > maxHeight)){
                 maxHeight = bundle.height + bundle.z;
             }
         }
 
-
         double scaleZ = height/maxHeight;
-        double scaleX = width/(maxLength - minLength);
+        double scaleX = width/(maxWidth - minWidth);
 
         for (BundleDto presenter : allBundles) {
             double xPos = 0;
             double zPos = 0;
-            if (minLength < 0) {
-                xPos = (presenter.position.getX() + Math.abs(minLength)) * scaleX;
+            if (minWidth < 0) {
+                xPos = (presenter.position.getX() + Math.abs(minWidth)) * scaleX;
             }
             else {
-                xPos = (presenter.position.getX() - minLength) * scaleX;
+                xPos = (presenter.position.getX() - minWidth) * scaleX;
             }
 
             zPos = (maxHeight - presenter.z)*scaleZ;
@@ -87,9 +86,52 @@ public class ElevationViewPresenter extends Pane implements IPresenter {
         }
     }
 
+    private void drawBundlesAxisY() {
+        double height = getHeight();
+        double width = getWidth();
+        double minLength = allBundles.get(0).getY() - (allBundles.get(0).length/2);
+        double maxLength = allBundles.get(0).getY() + (allBundles.get(0).length/2);
+        double maxHeight = allBundles.get(0).z + allBundles.get(0).height;
+        for (BundleDto bundle : allBundles) {
+            if ((bundle.position.getY()-(bundle.length/2)) < minLength){
+                minLength = bundle.position.getY() - (bundle.length/2);
+            }
+            if((bundle.position.getY() + (bundle.length/2) > maxLength)){
+                maxLength = bundle.position.getY() + (bundle.length/2);
+            }
+            if ((bundle.height + bundle.z > maxHeight)){
+                maxHeight = bundle.height + bundle.z;
+            }
+        }
+
+        double scaleZ = height/maxHeight;
+        double scaleY = width/(maxLength - minLength);
+
+        for (BundleDto presenter : allBundles) {
+            double yPos = 0;
+            double zPos = 0;
+            if (minLength < 0) {
+                yPos = (presenter.position.getY() + Math.abs(minLength)) * scaleY;
+            }
+            else {
+                yPos = (presenter.position.getY() - minLength) * scaleY;
+            }
+
+            zPos = (maxHeight - presenter.z)*scaleZ;
+
+            BundlePresenter bundlePresenter = new BundlePresenter(presenter);
+            Rectangle rectangle = bundlePresenter.getRectangle();
+            rectangle.setWidth(presenter.length*scaleY);
+            rectangle.setHeight(presenter.height*scaleZ);
+            rectangle.setX(yPos - rectangle.getWidth()/2);
+            rectangle.setY(zPos - rectangle.getHeight());
+            getChildren().add(rectangle);
+        }
+    }
+
     public void draw() {
         getChildren().clear();
         if (!allBundles.isEmpty())
-            drawBundles();
+            drawBundlesAxisY();
     }
 }
